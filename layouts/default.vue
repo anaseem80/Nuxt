902e1874ffd639353bc62151c20104d3ba880986
anaseem80 <anaseemamin@gmail.com>
2024-01-29 21:14:56 +0200

    Remove analytics package from project

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,12 +12,10 @@
 
     <div class="continer mx-auto p-4">
         <slot />
-        <Analytics />
     </div>
 </template>
 
 <script setup>
-import { Analytics } from '@vercel/analytics/react';
 
 </script>
 
